b1b46d9d35f684b3585b54e20f77200f68cec2aa
Paul Talaga <talagap@uindy.edu>
2017-09-12 13:45:05 -0400

    PHP Include demo

---
+++ Sept12-PHPInclude/dog.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dog page</title>
+</head>
+<body>
+  
+  <?php include("menu.html"); ?>
+  
+  <h1>Dog page</h1>
+  
+</body>
+</html>
---
+++ Sept12-PHPInclude/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cat page</title>
+</head>
+<body>
+ <?php include("menu.html"); ?>
+  <h1>Cat page</h1>
+ 
+</body>
+</html>
